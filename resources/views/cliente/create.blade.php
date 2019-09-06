73e0c1680c4975d7a043561d36d33b759f802164
nvila <nvila@live.com.ar>
2019-09-05 21:54:16 -0300

    se modifico la vista nuevo cliente de acuerdo a los requerimientos. Tambien se modifico en la base de datos los campos de la tabla cliente

--- resources/views/cliente/create.blade.php
+++ resources/views/cliente/create.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 @section('content')
-<!-- End Navbar -->
-<div class="content">
-    <div class="container-fluid">
-        <div class="section-image" data-image="../../assets/img/bg5.jpg">
-            <div class="container">
-                {{ Form::open(array('route' => 'clientes.store', 'files'=>true,'class'=>'form')) }}
+    <!-- End Navbar -->
+    <div class="content">
+        <div class="container-fluid">
+            <div class="section-image" data-image="../../assets/img/bg5.jpg">
+                <div class="container">
+                    {{ Form::open(array('route' => 'clientes.store', 'files'=>true,'class'=>'form')) }}
                     <div class="row">
                         <div class="col-md-8 col-sm-6">
                             <div class="card ">
@@ -16,124 +16,134 @@
                                 </div>
                                 <div class="card-body ">
                                     <div class="row">
-                                        <div class="col-md-5 pr-1">
+                                        <div class="col-md-6">
                                             <div class="form-group">
-                                                {{ Form::label('nombre', 'Nombres') }}
-                                                {{ Form::text('nombre', '', array('class' => 'form-control')) }}
+                                                <label>Nombres</label>
+                                                <input name="nombre" id="nombre" class="form-control" required>
                                             </div>
                                         </div>
-                                        <div class="col-md-3 px-1">
+                                        <div class="col-md-6">
                                             <div class="form-group">
-                                                {{ Form::label('apellido', 'Apellidos') }}
-                                                {{ Form::text('apellido', '', array('class' => 'form-control')) }}
+                                                <label>Apellido</label>
+                                                <input name="apellido" id="apellido" class="form-control" required>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                {{--{{ Form::label('cedula', 'Cedula') }}--}}
+                                                {{--{{ Form::text('cedula', '', array('class' => 'form-control')) }}--}}
+                                                <label>DNI</label>
+                                                <input name="cedula" id="cedula" type="number" class="form-control"
+                                                       required>
                                             </div>
                                         </div>
                                         <div class="col-md-4 pl-1">
                                             <div class="form-group">
-                                                {{ Form::label('sexo', 'Sexo') }}
+                                                <label>Fecha de nacimiento</label>
+                                                <input name="fechaN" id="datetimepicker" class="form-control datepicker"
+                                                       required>
+                                                {{--{{ Form::label('fechaN', 'Fecha de nacimiento') }}--}}
+                                                {{--{{ Form::text('fechaN', '', ['id' =>'datetimepicker', 'class' => 'form-control datepicker']) }}--}}
+                                            </div>
+                                        </div>
+                                        {{--<div class="col-md-3 pl-1">--}}
+                                        {{--<div class="form-group">--}}
+                                        {{--{{ Form::label('celular', 'Celular') }}--}}
+                                        {{--{{ Form::text('celular', '', array('class' => 'form-control')) }}--}}
+                                        {{--</div>--}}
+                                        {{--</div>--}}
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label>Sexo</label>
+                                                <select name="sexo" class="selectpicker" data-menu-style='dropdown-blue'
+                                                        data-style='btn-default btn-outline' required>
+                                                    <option value="" selected disabled>Seleccione un sexo</option>
+                                                    <option value="1">Masculino</option>
+                                                    <option value="2">Femenino</option>
+                                                    {{--<option disabled>Seleccione un sexo</option>--}}
+                                                </select>
+                                                {{--{{ Form::label('sexo', 'Sexo') }}--}}
+                                                {{--{{Form::select(--}}
+                                                {{--'sexo', ['m' => 'M', 'f'=> 'F', 'otro' => 'Otro'], null, [--}}
+                                                {{--'class' => 'selectpicker', --}}
+                                                {{--'data-title' => 'Seleccionar',--}}
+                                                {{--'data-style' => 'btn-default btn-outline', --}}
+                                                {{--'data-menu-style' => 'dropdown-blue'--}}
+                                                {{--])--}}
+                                                {{--}}--}}
+                                            </div>
+                                        </div>
+
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                {{ Form::label('civil', 'ESTADO CIVIL') }}
                                                 {{Form::select(
-                                                    'sexo', ['m' => 'M', 'f'=> 'F', 'otro' => 'Otro'], null, [
-                                                        'class' => 'selectpicker', 
+                                                    'civil', ['soltero' => 'Soltero', 'casado' => 'Casado', 'union libre' => 'Union Libre', 'divorciado' => 'Divorciado', 'viudo' => 'Viudo'], null, [
+                                                        'class' => 'selectpicker',
                                                         'data-title' => 'Seleccionar',
-                                                        'data-style' => 'btn-default btn-outline', 
+                                                        'data-style' => 'btn-default btn-outline',
                                                         'data-menu-style' => 'dropdown-blue'
                                                     ])
                                                 }}
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-3 pr-1">
-                                            <div class="form-group">
-                                                {{ Form::label('cedula', 'Cedula') }}
-                                                {{ Form::text('cedula', '', array('class' => 'form-control')) }}
-                                            </div>
-                                        </div>
-                                        <div class="col-md-3 pl-1">
-                                            <div class="form-group">
-                                                {{ Form::label('fechaN', 'Fecha de nacimiento') }}
-                                                {{ Form::text('fechaN', '', ['id' =>'datetimepicker', 'class' => 'form-control datepicker']) }}
-                                            </div>
-                                        </div>
-                                        <div class="col-md-3 pl-1">
-                                            <div class="form-group">
-                                                {{ Form::label('celular', 'Celular') }}
-                                                {{ Form::text('celular', '', array('class' => 'form-control')) }}
-                                            </div>
-                                        </div>
-                                        <div class="col-md-3 pl-1">
-                                            <div class="form-group">
-                                                {{ Form::label('tel', 'Telefono') }}
-                                                {{ Form::text('tel', '', array('class' => 'form-control')) }}
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-3">
-                                            <div class="form-group">
-                                            {{ Form::label('vivienda', 'Vivienda') }}
-                                                {{Form::select(
-                                                    'vivienda', ['propia' => 'Propia','alquiler' => 'Alquiler', 'otra' => 'Otra'], null, [
-                                                        'class' => 'selectpicker', 
-                                                        'data-title' => 'Seleccionar',
-                                                        'data-style' => 'btn-default btn-outline', 
-                                                        'data-menu-style' => 'dropdown-blue'
-                                                    ])
-                                                }}
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6">
+                                        <div class="col-md-8">
                                             <div class="form-group">
                                                 {{ Form::label('direccion', 'Direccion') }}
                                                 {{ Form::text('direccion', '', array('class' => 'form-control')) }}
                                             </div>
                                         </div>
-                                        <div class="col-md-3">
+
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-6">
                                             <div class="form-group">
-                                                {{ Form::label('civil', 'ESTADO CIVIL') }}
-                                                {{Form::select(
-                                                    'civil', ['soltero' => 'Soltero', 'casado' => 'Casado', 'union libre' => 'Union Libre', 'divorciado' => 'Divorciado', 'viudo' => 'Viudo'], null, [
-                                                        'class' => 'selectpicker', 
-                                                        'data-title' => 'Seleccionar',
-                                                        'data-style' => 'btn-default btn-outline', 
-                                                        'data-menu-style' => 'dropdown-blue'
-                                                    ])
-                                                }}
+                                                <label>Provincia</label>
+                                                <select name="localidad" class="selectpicker" data-menu-style='dropdown-blue'
+                                                        data-style='btn-default btn-outline' required>
+                                                    <option value="" selected disabled>Seleccione una Provincia</option>
+                                                    <option value="1">Tucuman</option>
+                                                    <option value="2">Jujuy</option>
+                                                </select>
+
+
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label>Localidad</label>
+                                                <select name="localidad" class="selectpicker" data-menu-style='dropdown-blue'
+                                                        data-style='btn-default btn-outline' required>
+                                                    <option value="" selected disabled>Seleccione una localidad</option>
+                                                    <option value="1">San Miguel de Tucuman</option>
+                                                    <option value="2">Monteros</option>
+                                                    <option value="2">Concepcion</option>
+                                                    <option value="2">Aguilares</option>
+                                                </select>
+
+
                                             </div>
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <div class="col-md-4 pr-1">
-                                            <div class="form-group">
-                                                {{ Form::label('empleo', 'Empleo') }}
-                                                {{Form::select(
-                                                    'empleo', ['empleado' => 'Empleado', 'desempleado' => 'Desempleado', 'estudiante' => 'Estudiante', 'otro' => 'Otro'], null, [
-                                                        'class' => 'selectpicker', 
-                                                        'data-title' => 'Seleccionar',
-                                                        'data-style' => 'btn-default btn-outline', 
-                                                        'data-menu-style' => 'dropdown-blue'
-                                                    ])
-                                                }}
-                                            </div>
-                                        </div>
-                                        <div class="col-md-4 px-1">
-                                            <div class="form-group">
-                                               {{ Form::label('ingreso', 'Trabajo ingreso') }}
-                                                {{ Form::number('ingreso', '', array('class' => 'form-control')) }}
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-4">
-                                            <div class="form-group">
-                                                {{ Form::label('referenciaPersonal', 'Referencia personal') }}
-                                                {{ Form::text('referenciaPersonal', '', array('class' => 'form-control')) }}
-                                            </div>
-                                        </div>
-                                        <div class="col-md-4">
+
+                                        <div class="col-md-6">
                                             <div class="form-group">
                                                 {{ Form::label('telR', 'Telefono') }}
                                                 {{ Form::text('telR', '', array('class' => 'form-control')) }}
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label>Email</label>
+                                                <input name="email" id="email" type="text" class="form-control"
+                                                       >
                                             </div>
                                         </div>
                                     </div>
@@ -150,39 +160,41 @@
                                         <label for="imageUpload"></label>
                                     </div>
                                     <div class="avatar-preview">
-                                        <div id="imagePreview" style="background-image: url('../assets/img/default-avatar.png');">
+                                        <div id="imagePreview"
+                                             style="background-image: url('../assets/img/default-avatar.png');">
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                {{ Form::close() }}
+                    {{ Form::close() }}
+                </div>
             </div>
         </div>
     </div>
-</div>
-<script>
-    function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            $('#imagePreview').css('background-image', 'url('+e.target.result +')');
-            $('#imagePreview').hide();
-            $('#imagePreview').fadeIn(650);
+    <script>
+        function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
+                    $('#imagePreview').hide();
+                    $('#imagePreview').fadeIn(650);
+                }
+                reader.readAsDataURL(input.files[0]);
+            }
         }
-        reader.readAsDataURL(input.files[0]);
-    }
-}
-$("#imageUpload").change(function() {
-    readURL(this);
-});
-$(document).ready(function(){
-    let fecha = new Date();
-    $('#datetimepicker').datetimepicker({
-            format: 'DD/MM/YYYY',
-            maxDate: fecha.setFullYear(fecha.getFullYear() - 18),
+
+        $("#imageUpload").change(function () {
+            readURL(this);
         });
-})
-</script>
+        $(document).ready(function () {
+            let fecha = new Date();
+            $('#datetimepicker').datetimepicker({
+                format: 'DD/MM/YYYY',
+                maxDate: fecha.setFullYear(fecha.getFullYear() - 18),
+            });
+        })
+    </script>
 @endsection
